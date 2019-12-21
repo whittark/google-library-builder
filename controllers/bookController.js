6c1c030219eccad654ff65e4f96a873dfad84328
Whitney Tarkington <whittark@gmail.com>
2019-12-21 13:59:43 -0800

    fixed case issues

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -13,9 +13,12 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
+    console.log(req.body)
     db.Book.create(req.body)
       .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
+      .catch(err => 
+        { console.log(err)
+          res.status(422).json(err)});
   },
   update: function(req, res) {
     db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
--- models/Book.js
+++ models/Book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   subtitle: { type: String },
-  authors: { type: [String], required: true },
+  authors: [{ type: String, required: true }],
   link: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
